e8bec285bbb56ede380b59410bf9b988b10c4811
Milan Koovacevic <m_kovacevic_@hotmail.com>
2016-10-13 18:14:10 +0200

    Worrking on company controller

--- src/main/java/rs/odnesi/controller/CompanyController.java
+++ src/main/java/rs/odnesi/controller/CompanyController.java
@@ -4,12 +4,12 @@ package rs.odnesi.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 import rs.odnesi.model.Company;
 import rs.odnesi.services.CompanyService;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -22,12 +22,36 @@ public class CompanyController {
     CompanyService companyService;
 
     @RequestMapping("/companyList")
-    public @ResponseBody List<Company> getCompanyList(Model model){
-        List<Company> companyList=companyService.getCompanyList();
-        model.addAttribute("companyList",companyList);
+    public
+    @ResponseBody
+    List<Company> getCompanyList(Model model) {
+        List<Company> companyList = companyService.getCompanyList();
+        model.addAttribute("companyList", companyList);
         return companyList;
 
     }
 
+    @RequestMapping("/company/{companyId}")
+    public String viewCompany(@PathVariable int companyId, Model model) {
+        Company company = companyService.getCompanyById(companyId);
+        model.addAttribute("company", company);
+        return "company";
+    }
 
+    @RequestMapping("/company/addCompany")
+    public String addCompany(Model model) {
+        Company company = new Company();
+        model.addAttribute("company", company);
+        return "addCompany";
+    }
+
+    @RequestMapping(value = "/company/addCompany", method = RequestMethod.POST)
+    public String addCompanyPost(@ModelAttribute("company") Company company, BindingResult result
+            , HttpServletRequest request) {
+        if (result.hasErrors()) {
+            return "addCompany";
+        }
+        companyService.addCompany(company);
+        return "redirect:company/companyList";
+    }
 }
